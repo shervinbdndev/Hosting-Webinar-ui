3a91a624a5d57f953a5714572f93cf6579ba5b12
Shervin <68682979+shervinbdndev@users.noreply.github.com>
2023-02-27 11:41:56 +0330

    Delete lib directory

--- lib/components/components.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class WebinarColors {
-  static const Color background = Color(0xff0c0d11);
-  static const Color card = Color(0xff111217);
-  static const Color redAccent = Color(0xffb93753);
-  static const Color text = Color(0xff7f86a6);
-}
